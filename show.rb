4d8261de640551edf3c042f68acdfe49789d41b7
zhangruobin001 <zhangruobin001@ke.com>
2021-01-14 16:52:47 +0800

    <U+1F4E6> NEW: homebrew

---
+++ show.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Show < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Mrzrb/show/releases/download/0.12/show_0.12_darwin_amd64.tar.gz"
+  sha256 "071641f9fa0aad06e40136738b79820a3b29bc33cb5fa1f13d23c41411baa39d"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    bin.install "show"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test show`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
